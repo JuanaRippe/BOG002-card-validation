2b94e6ef45eb84126b70b838dba6a52b0496fbb0
Katherineparra02 <katherineparraestepa@gmail.com>
2021-03-12 04:05:23 -0500

    cambios css y html

--- src/index.js
+++ src/index.js
@@ -6,15 +6,10 @@ function validar() {
 }
 /* Función para guardar el número  */
 import validator from './validator.js';
-
 let numero = document.getElementById("creditCardNumber").value;
      localStorage.setItem("creditCardNumber", numero); 
      console.log (invertirNumero(numero));
+/* Función para reverar el número  "123" => ["1","2","3",] => ["3","2","1"] => con .join("") = "321"console.log(validator);*/
+function invertirNumero(numero)
+return (numero.toString().split("").reverse());
 }
-/* Función para reverar el número  "123" => ["1","2","3",] => ["3","2","1"] => con .join("") = "321"*/
-    function invertirNumero(numero){
-    return (numero.toString().split("").reverse());
-}
-  
-   
-console.log(validator);
