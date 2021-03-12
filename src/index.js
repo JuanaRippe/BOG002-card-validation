974413b181b40a090ab2d4491f6af4af26967fb6
JuanaRippe <juana.rippe.jscr@gmail.com>
2021-03-12 09:00:58 -0500

    Cambios Juana

--- src/index.js
+++ src/index.js
@@ -1,22 +1,42 @@
-//import validator from './validator.js';
+import validator from './validator.js';
 /* Función para guarar el nombre */
-function validar(validar){
+function validar(){         
+/* Creamos una variable para llamar al nombre */
     let nombre = document.getElementById("nombre").value;
+/* Lo guardamos en el localStorage */
      localStorage.setItem("nombre", nombre);
+/* Referimos la página siguiente */
      window.location.href="html/tip.html" 
     }
 
 /* Función para guardar el número  */
     function siguiente(){
-    let numero = document.getElementById("creditCardNumber").value;
-     localStorage.setItem("creditCardNumber", numero); 
-     console.log (invertirNumero(numero));
-     
+/* creamos una variable para guardar el número ingresado */    
+    let numero = document.getElementById("numeroi").value;
+/* Lo guardamos en el localStorage */
+     localStorage.setItem("numero:", numero); 
+/* Mostramos en la consola el número invetrido y en un array */
+     /* console.log (invertirNumero(numero)); */ 
+    
     }
-/* Función para reverar el número  "123" => ["1","2","3",] => ["3","2","1"] => con .join("") = "321"*/
+/* Función para volver el número un array  "123" => ["1","2","3"]*/
     function invertirNumero(numero){
-    return (numero.toString().split("").reverse());
+    let creditCardNumber = numero.split("")
+    let reversarNumero = creditCardNumber.reverse();
+    return reversarNumero
+  
+    }
+    let x = invertirNumero("123467")
+
+
+    console.log (x)
+    let multiplicacion = [0, 0];
+    let reversarNumero = "1,2,3"
+     for ( let i = 0; i<reversarNumero.length; i++ ){
+       if( i % 2 === 0);{
+
+       }
     }
   
-   
-//console.log(validator);
+  
+console.log(validator);
