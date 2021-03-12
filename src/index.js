5fa60a83b3a1e77155df021afe42167871103c4d
Katherineparra02 <katherineparraestepa@gmail.com>
2021-03-11 21:04:05 -0500

    cambios en css y html

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
-/// import validator from './validator.js';
 /// Función para guarar el nombre */
 function validar() {
-let nombre = document.getElementById("nombre").value;
-localStorage.setItem("nombre", nombre);
-     window.location.href="html/index.html" 
-    }
-   
+  let nombre = document.getElementById("nombre").value;
+  localStorage.setItem("nombre", nombre);
+  window.location.href = "html/tip.html" 
+}
 /* Función para guardar el número  */
-function siguiente(){
-    let numero = document.getElementById("creditCardNumber").value;
+import validator from './validator.js';
+
+let numero = document.getElementById("creditCardNumber").value;
      localStorage.setItem("creditCardNumber", numero); 
      console.log (invertirNumero(numero));
-     
-    }
+}
 /* Función para reverar el número  "123" => ["1","2","3",] => ["3","2","1"] => con .join("") = "321"*/
     function invertirNumero(numero){
     return (numero.toString().split("").reverse());
-    }
+}
   
    
-//console.log(validator);
+console.log(validator);
