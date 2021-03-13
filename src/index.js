493c61be051208fcf78637cf0bc3ae5e54902107
Katherineparra02 <katherineparraestepa@gmail.com>
2021-03-13 14:20:11 -0500

    Cambios de la tercera y organizar header,main,footer

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-/// Función para guarar el nombre */
+/// Función para guarar el nombre
 function validar() {
   let nombre = document.getElementById("nombre").value;
   localStorage.setItem("nombre", nombre);
-  window.location.href = "html/tip.html" 
+  window.location.href = "index.html" 
 }
-/* Función para guardar el número  */
-import validator from './validator.js';
-let numero = document.getElementById("creditCardNumber").value;
-     localStorage.setItem("creditCardNumber", numero); 
-     console.log (invertirNumero(numero));
-/* Función para reverar el número  "123" => ["1","2","3",] => ["3","2","1"] => con .join("") = "321"console.log(validator);*/
-function invertirNumero(numero)
-return (numero.toString().split("").reverse());
+
+//
+import validator from './validator' ;
+let validarnumero = document.getElementById("validarnumero");
+validarnumero.addEventListener("click") , ()  => {
 }
+
+validarnumero.addEventListener("click") , ()  => {
+Event.preventDefault();
+
+let numeroi = document.getElementById("creditCardNumber").value;
+
+let resultado = validator.isValid("validarnumero");
